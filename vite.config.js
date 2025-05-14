cb642efd5398e8fb994f48449f3c163852f78516
Ayush246801 <shresthaaayush863@gmail.com>
2025-05-14 10:17:07 +0545

    .

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/new-portfolio.git',
+  base: '/ayush-sresta.github.io/',
   plugins: [react(), tailwindcss()],
 })
